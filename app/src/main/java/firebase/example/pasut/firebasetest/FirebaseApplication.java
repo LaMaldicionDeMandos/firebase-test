6da1c522d3f298f591932301e8d6c6c4aba1832b
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-08-15 11:38:35 -0300

    create project

---
+++ app/src/main/java/firebase/example/pasut/firebasetest/FirebaseApplication.java
@@ -0,0 +1,9 @@
+package firebase.example.pasut.firebasetest;
+
+import android.app.Application;
+
+/**
+ * Created by marcelo on 8/15/14.
+ */
+public class FirebaseApplication extends Application {
+}
